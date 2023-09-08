befda0467ac9837f373fd20f20c8b5f1b93c4cdf
dfloaizab <65038210+dfloaizab@users.noreply.github.com>
2023-09-08 17:51:19 -0500

    Create Parcial1_Viernes.java

---
+++ Unidad1/Parcial1_Lunes/Parcial1_Viernes.java
@@ -0,0 +1,54 @@
+
+/**
+ * 
+ * @author Diego Loaiza
+ * Algoritmos y Programación 2 
+ * 2023B
+ *  
+ */
+public class Parcial1_Viernes {
+	
+	
+	static String leaderboard_names[] = {"Starfield","GTA V","League of Legends","Valorant","Apex Legends"};
+	static int leaderboard_data[][];
+	
+	//COMPLETAR:declarar las constantes que determinan el tamaño
+	//          de la matriz de datos
+	
+	//COMPLETAR: declarar variables para guardar las horas totales de 
+	//streaming y horas promedio:
+		
+	
+	public static void main(String[] args)
+	{
+		//inicializar matriz leaderboard data con 3 columnas y 5 filas
+		leaderboard_data = new int[][]; //Completar código
+		
+		//1. recorrido para calcular el total de horas de streaming
+		// recorrido en la fila respectiva
+		for(int fila=0; fila < NUM_FILAS;fila++ )
+		{
+			
+		}
+		
+		//2. Búsqueda del nombre del videojuego con mayor numero de
+		//   espectadores
+		//  (recorrido en la fila respectiva)
+		for(int fila=0; fila < NUM_FILAS; fila++)
+		{
+			
+		}
+		
+		
+		//3. Cálculo del promedio de horas transmitidas
+		for(int fila=0; fila < NUM_FILAS; fila++)
+		{
+			
+		}
+		
+		//4. Imprimir los 3 valores obtenidos en los puntos anteriores:
+		
+		
+	}
+
+}
