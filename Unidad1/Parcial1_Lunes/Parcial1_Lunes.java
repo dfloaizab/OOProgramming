3da1d91c62fcbfe375896256e389120ebe24fce4
dfloaizab <65038210+dfloaizab@users.noreply.github.com>
2023-09-11 17:43:02 -0500

    Update Parcial1_Lunes.java

--- Unidad1/Parcial1_Lunes/Parcial1_Lunes.java
+++ Unidad1/Parcial1_Lunes/Parcial1_Lunes.java
@@ -5,7 +5,7 @@
  * 2023B
  *  
  */
-public class Parcial1_Viernes {
+public class Parcial1_Lunes {
 	
 	
 	static String leaderboard_names[] = {"Starfield","GTA V","League of Legends","Valorant","Apex Legends"};
