1922df74ea735f6b38273368ed9835129bb1eed5
dfloaizab <65038210+dfloaizab@users.noreply.github.com>
2023-10-20 20:36:46 -0500

    Create Credencial.java

---
+++ Unidad2/GestorContraseñas/Credencial.java
@@ -0,0 +1,18 @@
+
+public class Credencial 
+{
+	
+	//atributos:
+	private String user, pwd;
+	private boolean activa;
+	
+	
+	//métodos:
+	
+	//constructor
+	
+	//getters, setters
+	
+	//otros...
+
+}
