cbf9c50e117c0e1b0621242886612fe30932bc1a
dfloaizab <65038210+dfloaizab@users.noreply.github.com>
2025-04-01 11:29:25 -0500

    Create Empleado.java

---
+++ 2025A/Unidad2/Empleado.java
@@ -0,0 +1,24 @@
+
+import java.util.Date;
+
+public class Empleado 
+{
+	
+	
+	//Atributos:
+	private String nombre, direccion, telefono, tipo, eps, fon_pensiones;
+	private float salario_base;
+	private Date fechaNac;
+	
+	
+	//Métodos:	
+	/**
+	 * Método constructor de Empleado.
+	 * Crea un objeto Empleado sin atributos inicializados.
+	 */
+	public Empleado()
+	{
+		
+	}
+
+}
