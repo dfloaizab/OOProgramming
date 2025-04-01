5e5a86fe8fe660f4c4517426e86e1e8e8d53104a
dfloaizab <65038210+dfloaizab@users.noreply.github.com>
2025-04-01 11:29:46 -0500

    Create Empresa.java

---
+++ 2025A/Unidad2/Empresa.java
@@ -0,0 +1,23 @@
+
+public class Empresa 
+{
+	
+	//Atributos:
+	public String NIT, Nombre, Direccion,Telefono,emai;
+	public float capital_inicial;
+	
+	
+	//Métodos:
+	
+	/**
+	 * Constructor de la clase empresa: crea un objeto de esta clase
+	 * sin atributos inicializados
+	 */
+	public Empresa()
+	{
+		
+	}
+	
+	
+
+}
