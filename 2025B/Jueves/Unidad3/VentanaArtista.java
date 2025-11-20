bd0ad30864ad4b6ce5d98b4b8a46d851be6a86d3
Diego Loaiza <65038210+dfloaizab@users.noreply.github.com>
2025-11-20 11:44:09 -0500

    Create VentanaArtista.java

---
+++ 2025B/Jueves/Unidad3/VentanaArtista.java
@@ -0,0 +1,18 @@
+package biblioteca.UI;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class VentanaArtista extends JFrame{
+
+
+public VentanaArtista()
+{
+     setTitle("Agregar Artista");
+        setSize(600, 400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
+}
+
+    
+}
